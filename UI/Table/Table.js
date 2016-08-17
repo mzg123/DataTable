be71f9100a4bcd0024e74e335fb77e2e7fccc25e
风子 <67715107@qq.com>
2016-08-17 15:19:16 +0800

    1
    1

--- UI/Table/Table.js
+++ UI/Table/Table.js
@@ -5,7 +5,7 @@ var tempFields=[
     {
         name:"name",
         text:"姓名",
-        click:"",
+        click:{c:"click",fn:null},
         width:"auto",
         backgroud:"white",
         color:"",
@@ -13,8 +13,8 @@ var tempFields=[
     },
     {
         name:"name",
-        text:"姓名",
-        click:"",
+        text:"性别",
+        click:{c:"delete",fn:null},
         width:"auto",
         backgroud:"white",
         color:"",
@@ -22,8 +22,8 @@ var tempFields=[
     },
     {
         name:"name",
-        text:"姓名",
-        click:"",
+        text:"职业",
+        click:{c:"detail",fn:null},
         width:"auto",
         backgroud:"white",
         color:"",
@@ -39,12 +39,14 @@ var tempFields=[
     dataSource:[],
     headerShow:true,
     footerShow:true,
-    currentPage:0,
-    totalPages:0,
+    currentPage:1,
+    totalPages:3,
     totalRecoreds:0,
     prePage:0,
     nextPage:0,
-    width:"600px",
+    width:"600",
+        rowHeight:30,
+        coloumWidth:200,
     pageCount:10,
          table:{},
     cache:{}
@@ -60,67 +62,158 @@ var templates='<div class="table">'+
 var DTable=function(options){
     var self=this;
     self.options=$.extend({},tmpConfig,options);
-    self.options.fields=tempFields;
+
+    //self.options.fields=tempFields;
     self.options.tableId?self.options.table=$(self.options.tableId):$(templates);
-    ///
+
 }
-function SetHeader(){
-    var self=this;
+function SetHeader(t){
+    var self=t;
+
     var header="<div>"+self.options.header.text+"</div>";
-    self.options.table.find("header").html(header);
+
+    self.options.table.find(".header").html(header);
 }
-function SetFields(){
-    var self=this;
+function SetFieldWidth(self) {
+
+    var fieldsWidth = 0;
+    var realWidth = "auto";
+    var scroll=false;
+    $(self.options.fields).each(function (i) {
+        self.options.fields[i].width == "auto" ? "" : fieldsWidth = fieldsWidth + self.options.fields[i].width;
+    });
+
+    fieldsWidth>0?fieldsWidth > self.options.width ? realWidth = fieldsWidth : realWidth = self.options.width / $(self.options.fields).length:"";
+    fieldsWidth > self.options.width ? scroll=true:"";
+    alert(fieldsWidth);
+    return {fieldWidth:realWidth=="auto"? self.options.width / $(self.options.fields).length:realWidth,scroll:scroll};
+}
+function SetFields(t){
+    var self=t;
     var fields="";
-    self.options.fields.each(function(i,v){
-        fields=fields+ "<div>"+ v.text+"</div>";
+    self.options.scroll=SetFieldWidth(self).fieldWidth;
+    $(self.options.fields).each(function(i){
+        fields=fields+ "<div style='width:"+self.options.fields[i].width+"px'>"+ self.options.fields[i].text+"</div>";
     });
-    self.options.table.find("tbody").find("fields").html(fields);
+    self.options.table.find(".tbody").css("height", self.options.pageCount*self.options.rowHeight+"px").find(".fields").html(fields);
+    self.options.table.css("width",self.options.width);
 }
-function FillData(){
-    var self=this;
+function FillDataFromAjax(dataSource){
+    $(dataSource).each(function(i,v){
+            var dataS="<div class='row'>";
+            $(self.options.fields).each(function(fi,fv){
+                if(fv.click.fn){
+                    dataS=dataS+ "<div value='' class='click "+fv.click.c+"' style='height:"+self.options.rowHeight+"px; width:"+self.options.coloumWidth+"px'>"+ v[fv.name]+"</div>";
+                }else{
+                    dataS=dataS+ "<div style='height:"+self.options.rowHeight+"px;width:"+self.options.coloumWidth+"px'>"+ v[fv.name]+"</div>";
+                }
+            })
+            dataS=dataS+"</div>";
+            dataSA=dataSA+dataS;
+
+    });
+    self.options.table.find(".tbody").find(".dataSource").html(dataSA);
+}
+function FillData(t,page){
+    var self=t;
     var dataSA="";
-    self.options.dataSource.each(function(i,v){
-        var dataS="<div class='row'>";
-        self.options.fields.each(function(fi,fv){
-            dataS=dataS+ "<div>"+ v[fv.name]+"</div>";
-        })
-        dataS=dataS+"</div>";
-        dataSA=dataSA+dataS;
+    var from= 0,to=10;
+    from=(page-1)*self.options.pageCount;
+    self.options.currentPage=page;
+    to=page*self.options.pageCount-1;
+   if(typeof self.options.dataSource == 'function'){
+       self.options.dataSource(from,to,FillDataFromAjax,t);
+   }else{
+       $(self.options.dataSource).each(function(i,v){
+           if(i>=from && i<=to){
+               var dataS="<div class='row'>";
+               $(self.options.fields).each(function(fi,fv){
+                   if(fv.click.fn){
+                       dataS=dataS+ "<div value='' class='click "+fv.click.c+"' style='height:"+self.options.rowHeight+"px;width:"+self.options.coloumWidth+"px'>"+ v[fv.name]+"</div>";
+                   }else{
+                       dataS=dataS+ "<div style='height:"+self.options.rowHeight+"px;width:"+self.options.coloumWidth+"px'>"+ v[fv.name]+"</div>";
+                   }
+               })
+               dataS=dataS+"</div>";
+               dataSA=dataSA+dataS;
+           }
+           else if(i>to){
+               return false;
+           }
+       });
+       self.options.table.find(".tbody").find(".dataSource").html(dataSA);
+   }
+
+
+    $(self.options.fields).each(function(fi,fv){
+        if(fv.click.fn){
+            $("."+fv.click.c).click(function(){
+                fv.click.fn.call(this)
+            });
+        }
     });
-    self.options.table.find("tbody").find("dataSource").html(dataSA);
+    RefreshFooter.call(t);
 }
-function SetFooter(){
+function RefreshFooter(){
     var self=this;
-    var footer="<div>"+"<div>首页</div>"+"<div>上一页</div>"+"<div>下一页</div>"+"<div>尾页</div>"+"<div>4/9页</div>"+"<div>转到</div>"+"<input type='text'  />"+"<div>页</div>"+"</div>";
-    self.options.table.find("tfooter").html(footer);
+
+    //第一页 首页和上一页按钮不可用
+    if(self.options.currentPage==1){
+        $("#prePage").attr("disabled","disabled");
+        $("#firstPage").attr("disabled","disabled");
+    }else{
+        $("#prePage").removeAttr("disabled");
+        $("#firstPage").removeAttr("disabled");
+    }
+    //最后一页 尾页和下一页不可用
+    if(self.options.totalPages<=self.options.currentPage){
+        $("#lastPage").attr("disabled","disabled");
+        $("#nextPage").attr("disabled","disabled");
+    }else{
+        $("#lastPage").removeAttr("disabled");
+        $("#nextPage").removeAttr("disabled");
+    }
+    $("#pageInfo").html(self.options.currentPage+"/"+self.options.totalPages+"页");
+}
+function SetFooter(t){
+    var self=t;
+    var footer="<div class='footer'>"+"<button id='firstPage'    >首页</button>"+"<button id='prePage'>上一页</button>"+"<button id='nextPage'>下一页</button>"+"<button id='lastPage'>尾页</button>"+"<div id='pageInfo'>4/9页</div>"+"<div >转到</div>"+"<input type='text' id='tP'  />"+"<div>页</div>"+"<button id='toPage'>GO</button>"+"</div>";
+    self.options.table.find(".tfooter").html(footer);
+}
+function IniEvents(){
+    var self=this;
+    $("#firstPage").click(function(){
+        FillData(self,1);
+    });
+    $("#prePage").click(function(){
+        FillData(self,self.options.currentPage-1);
+    });
+    $("#nextPage").click(function(){
+        FillData(self,self.options.currentPage+1);
+    });
+    $("#lastPage").click(function(){
+        FillData(self,self.options.totalPages);
+    });
+    $("#toPage").click(function(){
+        $("#tP").val()? FillData(self,$("#tP").val()):"";
+    });
 }
 $.extend(DTable.prototype,{
-    _init:function(){
+    init:function(){
      //设置header
-      SetHeader();
+      SetHeader(this);
      //设置fields
-        SetFields();
+        SetFields(this);
         //设置数据源
-        FillData();
+        FillData(this,2);
      //设置footer
+        SetFooter(this);
+        //初始化事件
+        IniEvents.call(this);
     },
     setData:function(data){
 
-    },
-    firstPage:function(){
-
-    },
-    lastPage:function(){
-
-    },
-    nextPage:function(){
-
-    },
-    pageTo:function(index){
-
-    },
-    _refreshData:function(){
-
     }
 });
+
+
