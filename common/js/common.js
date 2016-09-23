12110195fb55ee0ff50b1e985fe56a83e82e7028
苗志高 <miaozhigao@yingu.com>
2016-09-23 11:10:58 +0800

    1
    1

---
+++ common/js/common.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/9/23.
+ */
+var Config={
+    "SeajsBaseUrl":"http://localhost:63342/mzg/DataTable/common/js/" //admin/user/user_index.js
+}
+
+seajs.config({
+    base: Config.SeajsBaseUrl
+});
