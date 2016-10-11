0831ba3b8c40fbbfef0a08343c8fe37a1c0488f9
苗志高 <miaozhigao@yingu.com>
2016-10-11 16:00:38 +0800

    1
    1

--- server/yg/routes/auth/auth.js
+++ server/yg/routes/auth/auth.js
@@ -8,19 +8,19 @@ function startRoute(app){
 }
 function handleAuth(app,req,res,next,cookie){
     var cookieObj=app.lcookie.parse(cookie);
-    app.config.noAuthPath.indexOf(req.path)!=-1?next():cookieObj.ssid?auth(app,req,res,next,cookieObj):res.redirect('/login');
+    app.config.noAuthPath.indexOf(req.path)!=-1?next():cookieObj.SESSIONID?auth(app,req,res,next,cookieObj):res.redirect('/login');
 }
 function auth(app,req,res,next,cookieObj){
 
     var options=app.base.getRequestOptions("/auth","get",app.config.serverPort,null);
-    var data={ssid:cookieObj.ssid};
+    var data={ssid:cookieObj.SESSIONID};
     // app.httpRep.req(app,req,res,options,success,error,data);
     next();
 }
 var success=function  (lapp,lres,jsonData){
-    lres.render('index/'+lapp.config.main, jsonData);
+    //lres.render('index/'+lapp.config.main, jsonData);
 }
 var error=function (lapp,lres,errMsg){
-    lres.render('index/'+lapp.config.main, { what: 'best', who: 'me', muppets: [ 'Kermit444', 'Fozzie444', 'Gonzo44' ]  });
+    //lres.render('index/'+lapp.config.main, { what: 'best', who: 'me', muppets: [ 'Kermit444', 'Fozzie444', 'Gonzo44' ]});
 }
 module.exports = startRoute;
--- server/yg/routes/login/login.js
+++ server/yg/routes/login/login.js
@@ -1,8 +1,8 @@
 
 function login(app){
     app.route.get('/login', function(req, res, next) {
-
-        res.cookie("ssid","Ap4GTEq");
+//res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true });
+        res.cookie("SESSIONID","Ap4GTEq",{httpOnly: true});
         res.cookie("mzg","1234567895125876");
         res.render('login/login');
         //app.httpRep.req(app,req,res,options,success,error);
