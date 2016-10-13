547e94cf6cf98e46b5d6fa81dc1c5ec2bf514e52
苗志高 <miaozhigao@yingu.com>
2016-10-13 14:07:53 +0800

    1
    1

--- server/yg/common/dbhelper.js
+++ server/yg/common/dbhelper.js
@@ -1,67 +1,8 @@
-
-//var mysql   = require('mysql');
-//var connection = mysql.createConnection({
-//    host   : 'localhost',
-//    user   : 'me',
-//    password : 'secret',
-//    database : 'my_db'
-///////pool.escape()
-//});
-//
-//connection.connect();
-//
-//// 增加记录
-//client.query('insert into test (username ,password) values ("lupeng" , "123456")');
-//
-//// 删除记录
-//client.query('delete from test where username = "lupeng"');
-//
-//// 修改记录
-//client.query('update test set username = "pengloo53" where username = "lupeng"');
-//
-//// 查询记录
-//client.query("select * from test" , function selectTable(err, rows, fields){
-//    if (err){
-//        throw err;
-//    }
-//    if (rows){
-//        for(var i = 0 ; i < rows.length ; i++){
-//            console.log("%d\t%s\t%s", rows[i].id,rows[i].username,rows[i].password);
-//        }
-//    }
-//});
-//
-//connection.end();
-
-var mysql = require('mysql');
-var db={
-    lapp:null,
-    getConn:function (dbtype){
-       return this.connPool.getConnection(function(err, connection) {
-           if(err){
-               this.lapp.datelogger.info("ConnectedMySqlError:");
-               this.lapp.datelogger.trace(err);
-               return null;
-           }else{
-               return connection;
-           }
-        });
-    },
-    connPool:null,
-    ini:function(app,dbtype){
-        this.lapp=app;
-        this.connPool||(this.connPool  = mysql.createPool({
-            connectionLimit : 10,
-            host            : app.config.dbconfig.host,
-            user            : app.config.dbconfig.user,
-            password        : app.config.dbconfig.pw,
-            database        : app.config.dbconfig.database
-        }));
-        return this;
-    }
-}
+var mysql = require('mysqlhelper');
+var mongo = require('mongohelper');
 
 var dbhelper={
+    db:null,
     ldb:null,
     lapp:null,
     assist:{
@@ -73,60 +14,34 @@ var dbhelper={
         }
     },
     execSql:function(sql,dbtype){
-        var relust=null;
-        var conn=this.ldb.getConn(dbtype);
-        conn.query(sql, function(err, rows) {
-             if(err){
-                 this.lapp.datelogger.info("ConnectedMySqlError:");
-                 this.lapp.datelogger.trace(err);
-             }
-             else{
-                 relust=rows;
-             }
-            conn.release();
-         });
-        return relust;
+        switch(dbtype){
+            case 1 :
+               return mysqlini(this.lapp,dbtype).execSql(sql,dbtype);
+                break;
+            case 2 :
+                return mongoini(this.lapp,dbtype).execSql(sql,dbtype);
+                break;
+            default:
+                return mysqlini(this.lapp,dbtype).execSql(sql,dbtype);
+                break;
+        }
     },
 
     transaction:function(sqlarr,dbtype){
-        var mldb=this.ldb;
-        var connection=this.ldb.getConn(dbtype);
-        connection.beginTransaction(function(err) {
-            if (err) {
-                mldb.datelogger.info("TransactionError:");
-                mldb.datelogger.trace(err);
-                return false;
-            }else{
-                return sqlarr.every(function(item, index, arr) {
-                    connection.query(item, function(err, result) {
-                        if (err) {
-                             connection.rollback(function () {
-                                mldb.datelogger.info("TransactionError:");
-                                mldb.datelogger.trace(err);
-                            });
-                            return false;
-                        }
-                        if(index==arr.length-1){
-                            connection.commit(function(err) {
-                                if (err) {
-                                     connection.rollback(function() {
-                                         mldb.datelogger.info("LastTransactionError:");
-                                         mldb.datelogger.trace(err);
-                                    });
-                                    return false;
-                                }
-                                 return true;
-                            });
-                        }
-                    });
-                    return true;
-                });
-            }
-        });
+        switch(dbtype){
+            case 1 :
+                return mysql.ini(this.lapp,dbtype).transaction(sql,dbtype);
+                break;
+            case 2 :
+                return mongo.ini(this.lapp,dbtype).transaction(sql,dbtype);
+                break;
+            default:
+                return mysqlini(this.lapp,dbtype).transaction(sql,dbtype);
+                break;
+        }
     },
     ini:function(app,dbtype){
         this.lapp=app;
-        ldb=db.ini(app,dbtype);
     }
 }
 module.exports=dbhelper;
