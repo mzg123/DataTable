61dc52db000f7cb9c793aecc8e8efd8af102a452
苗志高 <miaozhigao@yingu.com>
2016-10-13 14:11:31 +0800

    1
    1

--- server/yg/common/mongohelper.js
+++ server/yg/common/mongohelper.js
@@ -1,3 +1,99 @@
-/**
- * Created by Administrator on 2016/10/12.
- */
+
+var mongohelper = require('mysql');
+var db={
+    lapp:null,
+    getConn:function (){
+        return this.connPool.getConnection(function(err, connection) {
+            if(err){
+                this.lapp.datelogger.info("ConnectedMySqlError:");
+                this.lapp.datelogger.trace(err);
+                return null;
+            }else{
+                return connection;
+            }
+        });
+    },
+    connPool:null,
+    ini:function(app){
+        this.lapp=app;
+        this.connPool||(this.connPool  = mysql.createPool({
+            connectionLimit : 10,
+            host            : app.config.dbconfig.host,
+            user            : app.config.dbconfig.user,
+            password        : app.config.dbconfig.pw,
+            database        : app.config.dbconfig.database
+        }));
+        return this;
+    }
+}
+
+var mongohelper={
+    ldb:null,
+    lapp:null,
+    assist:{
+        escape:function(sql){
+            var conn=this.ldb.getConn(),result;
+            result= conn.escape(sql);
+            conn.release();
+            return result;
+        }
+    },
+    execSql:function(sql){
+        var relust=null;
+        var conn=this.ldb.getConn();
+        conn.query(sql, function(err, rows) {
+            if(err){
+                this.lapp.datelogger.info("ConnectedMySqlError:");
+                this.lapp.datelogger.trace(err);
+            }
+            else{
+                relust=rows;
+            }
+            conn.release();
+        });
+        return relust;
+    },
+
+    transaction:function(sqlarr){
+        var mldb=this.ldb;
+        var connection=this.ldb.getConn(dbtype);
+        connection.beginTransaction(function(err) {
+            if (err) {
+                mldb.datelogger.info("TransactionError:");
+                mldb.datelogger.trace(err);
+                return false;
+            }else{
+                return sqlarr.every(function(item, index, arr) {
+                    connection.query(item, function(err, result) {
+                        if (err) {
+                            connection.rollback(function () {
+                                mldb.datelogger.info("TransactionError:");
+                                mldb.datelogger.trace(err);
+                            });
+                            return false;
+                        }
+                        if(index==arr.length-1){
+                            connection.commit(function(err) {
+                                if (err) {
+                                    connection.rollback(function() {
+                                        mldb.datelogger.info("LastTransactionError:");
+                                        mldb.datelogger.trace(err);
+                                    });
+                                    return false;
+                                }
+                                return true;
+                            });
+                        }
+                    });
+                    return true;
+                });
+            }
+        });
+    },
+    ini:function(app){
+        this.lapp=app;
+        ldb=db.ini(app);
+        return this;
+    }
+}
+module.exports=mongohelper;
--- server/yg/controller/index/indexcontroller.js
+++ server/yg/controller/index/indexcontroller.js
@@ -1,4 +1,4 @@
-
+var dbhelper = require('../../common/dbhelper');
 
 var  indexcontroller={
    getData:function(app){
