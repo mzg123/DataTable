25c4b38a3c775283e969e5964899253e15859aed
苗志高 <miaozhigao@yingu.com>
2016-11-30 17:38:51 +0800

    1
    1

--- server/yg/static/module/block/roller/example.js
+++ server/yg/static/module/block/roller/example.js
@@ -4,28 +4,63 @@ var Roller=require("../block/roller/roller.js");
 var rollerItem=[
     {
         imgUrl:"http://cdn.yingu.com/upload/image/20160928/2b94e8d8f4d702d5331fccbd387169dc.jpg"
-        ,width:600
+        ,width:1200
         ,href:"http://www.baidu.com"
     }
     , {
         imgUrl:"http://cdn.yingu.com/upload/image/20161125/2548b11109bbe48a442c3b8a0f023bc.jpg"
-        ,width:600
+        ,width:1200
         ,href:"http://www.baidu.com"
     }
     , {
         imgUrl:"http://cdn.yingu.com/upload/image/20161029/ed9cd9aa1a6895560932461d8de834fa.jpg"
-        ,width:600
+        ,width:1200
+        ,href:"http://www.baidu.com"
+    }
+    , {
+        imgUrl:"http://cdn.yingu.com/upload/image/20161125/2548b11109bbe48a442c3b8a0f023bc.jpg"
+        ,width:1200
+        ,href:"http://www.baidu.com"
+    }
+    , {
+        imgUrl:"http://cdn.yingu.com/upload/image/20161029/ed9cd9aa1a6895560932461d8de834fa.jpg"
+        ,width:1200
+        ,href:"http://www.baidu.com"
+    }
+    , {
+        imgUrl:"http://cdn.yingu.com/upload/image/20161125/2548b11109bbe48a442c3b8a0f023bc.jpg"
+        ,width:1200
+        ,href:"http://www.baidu.com"
+    }
+    , {
+        imgUrl:"http://cdn.yingu.com/upload/image/20161029/ed9cd9aa1a6895560932461d8de834fa.jpg"
+        ,width:1200
+        ,href:"http://www.baidu.com"
+    }
+    , {
+        imgUrl:"http://cdn.yingu.com/upload/image/20161125/2548b11109bbe48a442c3b8a0f023bc.jpg"
+        ,width:1200
         ,href:"http://www.baidu.com"
     }
     , {
         imgUrl:"http://cdn.yingu.com/upload/image/20160928/2b94e8d8f4d702d5331fccbd387169dc.jpg"
-        ,width:600
+        ,width:1200
         ,href:"http://www.baidu.com"
     }
 ]
+var option={
+    showFooter:true
+    ,showClick:true
+    ,rollerType:1
+    ,borderWidth:4
+    ,autoPlay:true
+    ,playInterver:3000
+    ,itemCount:1
+    ,conWidth:1200
+    ,hasCurrentClass:true
+}
 
-
-ReactDOM.render(<Roller items={rollerItem} rollerType={1} currentIndex={0} borderWidth={4}></Roller>, document.getElementById('rollerCon'));
+ReactDOM.render(<Roller items={rollerItem} option={option}  ></Roller>, document.getElementById('rollerCon'));
 
 
 
